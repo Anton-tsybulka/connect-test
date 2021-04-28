acbadfbeb049037b9520f8c5de44ff9bc2f2c55a
Anton Tsybulka <anton.kurto@gmail.com>
2021-04-28 20:25:19 +0300

    delete import

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,5 +1,5 @@
 import { all } from 'redux-saga/effects';
-import { getPeopleSaga, getImagesSaga, changeInputValue } from './peopleSaga';
+import { getPeopleSaga, changeInputValue } from './peopleSaga';
 
 export default function* rootSaga() {
    yield all([
